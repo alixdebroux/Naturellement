7f88be4584b0fede2128e09b3085063f4d2c8947
Alix <alix.debroux@gmail.com>
2023-05-22 19:32:48 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Naturellement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
